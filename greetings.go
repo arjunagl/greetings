5420def6276d6107db30e81a03057d4361b32aa9
Arjuna Marambe <arjunam@buildxact.com>
2021-02-25 07:49:50 +1100

    testing

--- greetings.go
+++ greetings.go
@@ -10,7 +10,7 @@ import (
 func Hello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	log.WithFields(log.Fields{
-		"animal": "simple logger 1.0.3",
+		"animal": "simple logger 2.0.0",
 	}).Info("A major logger version")
 
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
